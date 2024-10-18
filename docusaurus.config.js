e4fed30e19d85e334a7d689d2f297b460982e3ee
lailai0916 <lailai0x394@gmail.com>
2024-10-18 16:16:21 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -105,7 +105,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'lailai\'s blog',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
