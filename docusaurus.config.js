16777886ec67cc185fd09e16e080d5eff04bc986
lailai0916 <lailai0x394@gmail.com>
2024-10-23 08:48:59 +0800

    udpate

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -112,9 +112,6 @@ const config = {
     ({
       // Replace with your project's social card
       image: 'img/logo.svg',
-      sidebar: {
-        hideable: true,
-      },
       navbar: {
         title: 'lailai\'s Blog',
         logo: {
