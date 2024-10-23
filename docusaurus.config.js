da1aa8e508a37951fd08e668f5f87eed85e7c914
lailai0916 <lailai0x394@gmail.com>
2024-10-23 08:58:46 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -109,7 +109,6 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      hideableSidebar: true,
       image: 'img/logo.svg',
       navbar: {
         title: 'lailai\'s Blog',
